0901ee2f32e136eb18401c8e025c3a2cdc114579
Ivan <arias.ivan@gmail.com>
2023-07-24 00:30:14 -0400

    lab done

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive)
+VALUES
+    (1, "Mr. Chocolate", 5, "M", "Brown", "Angry", 1),
+    (2, "Rowdy", 7, "F", "Black", "Kind", 1),
+    (3, "Tabitha", 18, "F", "Blue", "Gentle", 1),
+    (4, "Sergeant Brown", 10, "M", "Black", "Sturdy", 1),
+    (5, "Melissa", 14, "F", "Brown", "Scheming", 0),
+    (6, "Grinch", 19, "M", "Black", "Nasty", 1),
+    (7, "Wendy", 11, "F", "Brown", "Sweet", 0),
+    (8, NULL, 31, "M", "Black", NULL, NULL);
